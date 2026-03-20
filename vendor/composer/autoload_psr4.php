3f372db7bf3aeaa2b4ce8f196bad0a9533c5329e
Benedict Massolle <bm@presentprogressive.de>
2026-03-20 15:18:36 +0100

    Fix broken composer install

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Kirby\\' => array($vendorDir . '/getkirby/composer-installer/src'),
 );
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -10,6 +10,15 @@
         'dev' => false,
     ),
     'versions' => array(
+        'getkirby/composer-installer' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../getkirby/composer-installer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'hananils/kirby-choices' => array(
             'pretty_version' => '4.0.0',
             'version' => '4.0.0.0',
